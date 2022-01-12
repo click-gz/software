a37d937d8fa721f132ca83081a11928cec64d733
click_pigz <2437954492@qq.com>
2022-01-12 20:04:50 +0800

    first commit

---
+++ Software_engineering_course/src/main/java/software/xmclass/Software_engineering/_course/SoftwareEngineeringCourseApplication.java
@@ -0,0 +1,13 @@
+package software.xmclass.Software_engineering._course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineeringCourseApplication.class, args);
+	}
+
+}
---
+++ Software_engineering_course/src/test/java/software/xmclass/Software_engineering/_course/SoftwareEngineeringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package software.xmclass.Software_engineering._course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineeringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
